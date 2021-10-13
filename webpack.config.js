d6be65d92d15348d8212562df7577ee993f1d86c
david <davidcejazapata@gmail.com>
2021-10-12 23:44:49 -0500

    Preparacion del entorno

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: path.resolve(__dirname,'./src/js/index.js'),
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.js'
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'main.html',
+            template: './src/html/main.html',
+            templateParameters: {
+                titulo: 'weather'
+            }
+        })
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.(s(a|c)ss)$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eott|ttf|otf)$/i,
+                type: 'asset/resource',
+            }
+        ],
+    },
+};
