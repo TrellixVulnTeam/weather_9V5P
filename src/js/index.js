5ffacf45ae8bd264794f9301adea73a4cda96cf5
david <davidcejazapata@gmail.com>
2021-10-14 13:37:27 -0500

    Layout completed, working in transitions

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,17 @@
 import '../scss/style.scss';
+
+const submitBtn = document.querySelector('.form--submit');
+const headerTittle = document.querySelector('.header--title');
+const headerImage = document.querySelector('.header--figure');
+
+submitBtn.addEventListener('click', checkWeather);
+
+function checkWeather() {
+    console.log('Hola');
+    /**
+     * When the submit button is presed:
+     * 1.- Remove the title
+     * 2.- Add the class transition to the image
+     */
+    headerTittle.remove();
+}
